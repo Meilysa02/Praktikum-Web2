61106def0310266b0368d85bda89cec578f2ab13
Meilysa02 <meilysamaulida02@gmail.com>
2025-06-03 09:37:09 +0800

    Initial commit

---
+++ profile.php
@@ -0,0 +1,52 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/melly.jpeg" alt="z" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2310010130</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Meilysa Maulida</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Kertak Hanyar, 2 Mei 2005</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">081256322500</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">meilysamaulida02@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/meilysamaulida" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/" target="_blank">
+                                <img src="img/medsos/linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
